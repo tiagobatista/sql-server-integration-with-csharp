811d99a39ad40ecbddf9b24e37894f9056e11a09
Tiago Batista <tiago.batista94@gmail.com>
2024-08-26 21:46:08 +0100

    Final solution

--- SQLServerIntegrationWithCSharp/Program.cs
+++ SQLServerIntegrationWithCSharp/Program.cs
@@ -1,17 +1,15 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
-using SQLServerIntegrationWithCSharp.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
 builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer("Server=localhost;Database=master;User Id=sa;Password=14610429Fcp.;Encrypt=True;TrustServerCertificate=True"));
 
 var app = builder.Build();
 
